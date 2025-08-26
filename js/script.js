6b06b5d2a03ece04b6b8183c0b1712eede7898c7
matheus <matheus.cbatista7@gmail.com>
2025-08-26 16:03:22 -0300

    Update Project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Animação
+function animarContador(id, valorFinal, duracao) {
+  const elemento = document.getElementById(id);
+  let inicio = 0;
+  const incremento = Math.ceil(valorFinal / (duracao / 16));
+  const intervalo = setInterval(() => {
+    inicio += incremento;
+    if (inicio >= valorFinal) {
+      elemento.textContent = valorFinal.toLocaleString("pt-BR");
+      clearInterval(intervalo);
+    } else {
+      elemento.textContent = inicio.toLocaleString("pt-BR");
+    }
+  }, 16);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  animarContador("contador", 10000, 4000); // 2 segundos
+});
